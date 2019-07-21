746f1ea1a0a3d2b28fb6af92d78df6b9479ecfb9
April Gustafson <apriljoy@Aprils-MacBook-Pro.local>
2019-07-21 15:21:01 -0400

    First commit

---
+++ TherAppy/src/Therapist.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+import java.util.List;
+
+public class Therapist {
+
+  private int therapistID;
+  private String lastName;
+  private String firstName;
+  private char gender;    // F, M, N
+  private Date dob;
+  private String zipCode;
+  private int costPerSession;
+  private String style;
+  private String qualification;
+  private List<String> insuranceAffiliations; //???????
+
+}
