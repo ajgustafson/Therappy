9f1ea569e5efac4833bc397efce5818a9780e6c5
Cassidy Bayen <cassidybayenc@gmail.com>
2019-07-29 17:32:38 -0400

    added setup for procedures

--- create_tables.sql
+++ create_tables.sql
@@ -129,3 +129,85 @@ CREATE TABLE user_answers_questions(
     PRIMARY KEY (user_id, question_id)
 );
 
+
+delimiter //
+-- This procedure adds a new user to the databse AFTER they have completed the questionaire
+
+create procedure addUser
+(
+	in first_name_param VARCHAR(50),
+    in last_name_param VARCHAR(50),
+    in username_param VARCHAR(50),
+    in email_param VARCHAR(100),
+    in pwd_param VARCHAR(20),
+    in gender_param ENUM('F', 'M'),
+    in zipcode_param CHAR(5),
+    in dob_param DATE,
+    in insurance_param VARCHAR(100),
+    in max_distance_param INT,
+    in gender_pref_param ENUM('F', 'M'),
+    in qualification_pref_param VARCHAR(100),
+    in needs_insurance_param boolean,
+    in question_1_param int,
+    in question_2_param int,
+    in question_3_param int,
+    in question_4_param int,
+    in question_5_param int
+)
+
+begin
+	-- variable declarations
+    declare style_pref_var int;
+    declare qualification_pref_id_var int;
+    declare insurance_id_var int;
+    declare user_id_var int;
+    
+end //
+delimiter ;
+
+-- This procedure finds all similar users to the current user
+delimiter //
+
+create procedure findSimilarUsers
+(
+	in param_name int  -- params go here, separated by commas
+)
+
+begin
+
+    declare var_name int; -- variable declarations, ending each lline with semicolons
+    
+end //
+delimiter ;
+
+
+-- This procedure finds all user/therapist matches
+delimiter //
+
+create procedure findMatchingTherapists
+(
+	in param_name int  -- params go here, separated by commas
+)
+
+begin
+
+    declare var_name int; -- variable declarations, ending each lline with semicolons
+    
+end //
+delimiter ;
+
+-- This procedure filters the list of user/therapist matches using 
+-- similar users then returns a list of the top 5
+delimiter //
+
+create procedure filterMatchingTherapists 
+(
+	in param_name int  -- params go here, separated by commas
+)
+
+begin
+
+    declare var_name int; -- variable declarations, ending each lline with semicolons
+    
+end //
+delimiter ;
