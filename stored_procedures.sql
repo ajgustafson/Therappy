8a58cf234141acf90a895ff210e92dc87520c3c9
jwtaylor18 <46580717+jwtaylor18@users.noreply.github.com>
2019-08-02 09:40:20 -0400

    add user procedures
    added procedures for capturing all information entered by user in questionaire and through rating therapist

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,37 +1,180 @@
-use Therappy;
+use therappy;
 
+-- A stored procedure to insert some information into the User table after the user completes the questionaire.  
+-- Inserts information that does not depend on any other tables in the User table.
+-- Information that depends on other tables is inserted as null (and will be updated via another procedure)
+drop procedure if exists insert_user_basics;
 
 delimiter //
--- This procedure adds a new user to the databse AFTER they have completed the questionaire
-create procedure addUser
+create procedure insert_user_basics
 (
-	in first_name_param VARCHAR(50),
-    in last_name_param VARCHAR(50),
-    in username_param VARCHAR(50),
-    in email_param VARCHAR(100),
-    in pwd_param VARCHAR(20),
-    in gender_param ENUM('F', 'M'),
-    in zipcode_param CHAR(5),
-    in dob_param DATE,
-    in insurance_param VARCHAR(100),
-    in max_distance_param INT,
-    in gender_pref_param ENUM('F', 'M'),
-    in qualification_pref_param VARCHAR(100),
-    in needs_insurance_param boolean,
-    in question_1_param int,
-    in question_2_param int,
-    in question_3_param int,
-    in question_4_param int,
-    in question_5_param int
+    in first_name varchar(255),
+    in last_name varchar(255),
+    in username varchar(255),
+    in pword varchar(255),
+    in date_of_birth date,
+    in gender varchar(255),
+	in email varchar(255),
+    in zipcode char(5),
+    in max_distance int,
+    in gender_pref varchar(255),
+    in max_cost int,
+    in needs_insurance tinyint
 )
-
 begin
-	-- variable declarations
-    declare style_pref_var int;
-    declare qualification_pref_id_var int;
+
+    insert into user (first_name, last_name, username, pwd, dob, gender, email, zipcode, insurance_id, max_distance, gender_pref, max_cost, qualification_pref, needs_insurance, style_pref) values(first_name, last_name, username, pword, date_of_birth, gender, email, zipcode, null, max_distance, gender_pref, max_cost, null, needs_insurance, null);
+
+end //
+delimiter ;
+
+-- A stored procedure to update (as it was initially set to null) the user's insurance info in the User table.  
+-- Creates a new entry in the Insurance table if insurance does noe exist yet
+drop procedure if exists insert_user_insurance;
+
+delimiter //
+create procedure insert_user_insurance
+(
+	in email_param varchar(255),
+    in insurance_param varchar(255)
+)
+begin
+	
     declare insurance_id_var int;
-    declare user_id_var int;
+        
+if insurance_param in (select name from insurance) then
+	 select insurance_id into insurance_id_var from insurance where name = insurance_param;
+else
+	insert into insurance (name) values (insurance_param);
+    select insurance_id into insurance_id_var from insurance where name = insurance_param;
+end if;
     
+update user
+set insurance_id = insurance_id_var
+where email = email_param;
+
+end //
+delimiter ;
+
+-- Procedure to update the user's qualification preference (as it was initially set to null) in the User table
+-- If the qual_param does not match exactly an entry in the Qualification table, qaulification_pref will remain as null
+-- in the User table
+drop procedure if exists insert_user_qual_pref;
+
+delimiter //
+create procedure insert_user_qual_pref
+(
+	in email_param varchar(255),
+    in qual_param varchar(255)
+)
+begin
+    declare qual_id_var int;
+    set qual_id_var = null;
+        
+if qual_param in (select name from qualification) then
+	 select qualification_id into qual_id_var from qualification where name = qual_param;
+end if;
+    
+update user
+set qualification_pref  = qual_id_var
+where email = email_param;
+
+end //
+delimiter ;
+
+-- Procedure to insert user's maladies into the user_exhibits_malady table.  Malady_param must match exactly 
+-- to an entry in the malady table.
+drop procedure if exists insert_user_malady;
+
+delimiter //
+create procedure insert_user_malady
+(
+	in email_param varchar(255),
+    in malady_param varchar(255)
+)
+begin
+    declare user_id_var int;
+    declare malady_id_var int;
+        
+select user_id into user_id_var from user where email = email_param;
+
+if malady_param in (select name from malady) then
+	select malady_id into malady_id_var from malady where name = malady_param;
+    insert into user_exhibits_malady (user_id, malady_id) values (user_id_var, malady_id_var);
+end if;
+
+end //
+delimiter ;
+
+
+-- Procedure to insert the user's responses to the 5 questions at the end of the survey related to style preference.
+drop procedure if exists insert_response;
+
+delimiter //
+create procedure insert_response
+(
+	in email_param varchar(255),
+    in choice_id_param int
+)
+begin
+    declare user_id_var int;
+        
+select user_id into user_id_var from user where email = email_param;
+
+insert into user_makes_choices(user_id, choice_id) values (user_id_var, choice_id_param);
+end //
+delimiter ;
+
+-- Procedure that should be called after the user's responses to the style pref questions have been inserted into the table
+-- Determines the user's style pref and inserts this entry into the 'style_pref' column in the User table
+drop procedure if exists update_style;
+
+delimiter //
+create procedure update_style
+(
+	in email_param varchar(255)
+)
+begin
+    declare user_id_var int;
+    declare total_sum_var int;
+    declare style_id_var int;
+        
+select user_id into user_id_var from user where email = email_param;
+
+select SUM(value) into total_sum_var from user_makes_choices u left join choice c on (u.choice_id = c.choice_id) where user_id = user_id_var; 
+
+if (total_sum_var > 0) then
+	select style_id into style_id_var from style where name = 'behavoiral' limit 1;
+	update user
+	set style_pref = style_id_var
+	where email = email_param;
+end if;
+if (total_sum_var < 0) then
+	select style_id into style_id_var from style where name = 'procedural' limit 1;
+	update user
+	set style_pref = style_id_var
+	where email = email_param;
+end if;
+	
+end //
+delimiter ;
+
+-- Procedure to insert a user's rating of a therapist into the 'user_rates_therapist table'
+drop procedure if exists insert_therapist_rating;
+
+delimiter //
+create procedure insert_therapist_rating
+(
+	in email_param varchar(255),
+    in therapist_id_param int,
+    in rating_param int
+)
+begin
+    declare user_id_var int;
+        
+select user_id into user_id_var from user where email = email_param;
+
+insert into user_rates_therapist(user_id, therapist_id, rating) values (user_id_var, therapist_id_param, rating_param);
 end //
 delimiter ;
 
