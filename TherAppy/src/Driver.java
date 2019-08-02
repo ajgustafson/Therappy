919a029814688f69ec31feb1ca57ba12bf651169
Jeff Taylor <jeff.taylor@slalom.com>
2019-08-02 09:46:21 -0400

    Add Driver

--- TherAppy/src/Driver.java
+++ TherAppy/src/Driver.java
@@ -1,75 +1,53 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import JDBC_utils.DBUtils;
 
 public class Driver {
 
-  static DBUtils dbu;
-
-//  private String[] getUserAndPassword() {
-//    System.out.println("Please login to the database:");
-//    System.out.print("url: ");
-//    Scanner scanner = new Scanner(System.in);
-//    String userName = scanner.nextLine();
-//
-//    System.out.print("username: ");
-//    Scanner scanner = new Scanner(System.in);
-//    String userName = scanner.nextLine();
-//
-//    System.out.print("password: ");
-//    scanner = new Scanner(System.in);
-//    String password = scanner.nextLine();
-//    String[] data = {url, userName, password};
-//    return data;
-//  }
-
-  private static List<String> getSquirrels() {
-    String sql = "select * from squirrel";
-
-    List<String> squirrels = new ArrayList<String>();
-
-
-    try {
-      // get connection and initialize statement
-      Connection con = dbu.getConnection();
-      Statement stmt = con.createStatement();
-      ResultSet rs = stmt.executeQuery(sql);
-      while (rs.next() != false)
-        squirrels.add(
-                rs.getString("squirrel_id") + " " +
-                        rs.getString("name") + " " +
-                        rs.getString("gender") + " "
-        );
-      rs.close();
-      stmt.close();
-    } catch (SQLException e) {
-      System.err.println(e.getMessage());
-      e.printStackTrace();
-    }
-
-    return squirrels;
-  }
+  private static DBUtils dbu;
 
   public static void main(String[] args) {
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-    } catch (Exception e) {
-      //
-    }
-    dbu = new DBUtils("jdbc:mysql://localhost:3306/squirrels?serverTimezone=EST5EDT",
-            "therappy", "therappyproject!");
 
-    List<String> squirrels = getSquirrels();
 
-    for (String str : squirrels) {
-      System.out.println(str);
-    }
+    dbu = new DBUtils("jdbc:mysql://localhost:3306/therappy",
+            "insert your username here", "insert your password here");
 
-    dbu.closeConnection();
+    TherAppyAPI api = new API(dbu);
+
+    List<String> malady1;
+    malady1 = new ArrayList<>();
+    malady1.add("Addiction");
+    malady1.add("Career");
+    malady1.add("Bipolar");
+
+    User u1 = new User("bob", "bob", "jefe101", "pword101", "bob@testing.com",
+            "M", "2017-04-03",
+            "02131", "aetna", malady1, 8, "F",
+            6, "MA", 0, 1);
+
+    User u2 = new User("jake", "jake", "jefe101", "pword101", "jake@testing.com",
+            "M", "2017-04-03",
+            "02131", "aetna", new ArrayList<>(), 8, "F",
+            6, "PhD", 0, 1);
+
+    User u3 = new User("april1", "april1", "jefe101", "pword101", "april1@testing.com",
+            "M", "2017-04-03",
+            "02131", "cigna", new ArrayList<>(), 8, "F",
+            6, "MS", 0, 1);
+
+
+    api.insertUser(u1);
+    api.insertStylePrefResponse(u1,40001);
+    api.insertStylePrefResponse(u1,40003);
+    api.insertStylePrefResponse(u1,40005);
+    // two more insertStylePrefResponse here
+
+    api.updateUserStyle(u1);
+    api.insertTherapistRating(u1,1001, 7);
   }
 }
